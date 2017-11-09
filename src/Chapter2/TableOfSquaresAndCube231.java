e6f7b5dce5f855e4c0f25400dc499a3271b26328
Gavrisciuc Vladislav <angelbeats9632@gmail.com>
2017-11-09 13:30:23 +0200

    Chapter2 added

--- src/Chapter2/TableOfSquaresAndCube231.java
+++ src/Chapter2/TableOfSquaresAndCube231.java
@@ -1,9 +1,9 @@
 package Chapter2;
 public class TableOfSquaresAndCube231 {
     public static void main(String[] args){
-        System.out.printf("number\tsquare\tcube%n");
+        System.out.printf("num\t|\tsqr\t|\tcube%n");
         for(int i = 0; i<= 10; i++){
-            System.out.printf("%d\t\t%d\t\t%d%n", i, i*i, i*i*i);
+            System.out.printf("%d\t|\t%d\t|\t%d%n", i, i*i, i*i*i);
         }
     }
 }
